e6c9114e69e26c6b6ce1dc9e0c131929fc59816d
Bartek <bartlomiej.kacki@gmail.com>
2024-07-22 20:20:08 +0200

    Add project files.

---
+++ LINQtest2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQtest2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sandwiches = new[] { "Szynka i ser", "Salami i majonez", "Indyk i ser", "Kotlet z kurczaka" };
+
+            var sandwichesOnRye =
+                from sandwich in sandwiches
+                select $"{sandwich} na chlebie ryżowym";
+
+            foreach (var sandwich in sandwichesOnRye)
+                Console.WriteLine(sandwich);
+        }
+    }
+}
