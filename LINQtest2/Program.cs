9e83c9b3ef5fda1503f95ac6b9e61e88a7f73493
Bartek <bartlomiej.kacki@gmail.com>
2024-07-23 19:35:44 +0200

    Added sequence computations example

--- LINQtest2/Program.cs
+++ LINQtest2/Program.cs
@@ -12,6 +12,24 @@
 
             foreach (var sandwich in sandwichesOnRye)
                 Console.WriteLine(sandwich);
+
+            Console.WriteLine();
+
+            var random = new Random();
+            var numbers = new List<int>();
+            int length = random.Next(10, 25);
+            for(int i=0; i < length; i++)
+                numbers.Add(random.Next(100));
+            Console.WriteLine($@"Statystyki dla tych {numbers.Count()} liczb:
+Pięć pierwszych liczb: {String.Join(", ", numbers.Take(5))}
+Pięć ostatnich liczb: {String.Join(", ", numbers.TakeLast(5))}
+Pierwsza liczba: {numbers.First()}; ostatnia liczba {numbers.Last()}
+Najmniejsza liczba: {numbers.Min()}; ostatnia liczba {numbers.Last()}
+Suma: {numbers.Sum()}
+Średnia: {numbers.Average():F2}
+Liczby: {String.Join(", ", numbers)}");
+
+            Console.WriteLine();
         }
     }
 }
